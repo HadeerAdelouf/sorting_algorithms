938e6ac60f5582bbb1d573e2c5f0077a93f13564
HadeerAdelouf <hadiradel999@gmail.com>
2023-12-17 08:18:18 +0200

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,52 @@
-#include"sort.h"
-
+#include "sort.h"
 /**
- * Swap_Nodes - swap 2 nodes in double LS
- * @first : first node address
- * @second :second node address
- * Return : void
+ * swap_nodes - swap 2 element in an list
+ * @h: head of list
+ * @n1: node
+ * @n2: node
  */
-void Swap_Nodes(listint_t *first, listint_t *second)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	if (first->prev)
-		first->prev->next = second;
-	if (second->next)
-		second->next->prev = first;
-	first->next = second->next;
-	second->prev = first->prev;
-	first->prev = second;
-	second->next = first;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
+
 /**
- * insertion_sort_list - function that sorts a doubly linked list
- * of integers in ascending order using the Insertion sort algorithm
- * @list: Dobule linked list to sort
+ * insertion_sort_list  - insertion_sort_list
+ * @list: doubly liked list
+ *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *x, *j;
+	listint_t *head, *prev;
+	int value;
 
-	if (!(*list)->next || !list || !*list)
-		return;
-	x = (*list)->next;
-	while (x)
+	if (list == NULL || (*list)->next == NULL || (*list) == NULL)
 	{
-		j = x;
-		x = x->next;
-		while (j && j->prev)
+		return;
+	}
+	head = *list;
+	while (head)
+	{
+		prev = head->prev;
+				value = head->n;
+
+		while (prev && prev->n > value)
 		{
-			if (j->prev->n > j->n)
-			{
-				Swap_Nodes(j->prev, j);
-				if (!j->prev)
-					*list = j;
-				print_list((const listint_t *)*list);
-			}
-			else
-				j = j->prev;
+			swap_nodes(prev, head, list);
+			print_list(*list);
+			prev = head->prev;
 		}
+		head = head->next;
 	}
 }
