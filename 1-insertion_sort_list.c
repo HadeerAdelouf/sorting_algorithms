270a820b6328e6276154b96d26b831cba12ead50
HadeerAdelouf <hadiradel999@gmail.com>
2023-12-17 08:25:33 +0200

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,52 @@
 #include "sort.h"
+
 /**
- * swap_nodes - swap 2 element in an list
- * @h: head of list
- * @n1: node
- * @n2: node
+ * swap_node - Swap two nodes in doubly-linked list.
+ * @head: head of the doubly-linked list.
+ * @a: first node to swap.
+ * @b:second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_node(listint_t **head, listint_t **a, listint_t *b)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*a)->next = b->next;
+	if (b->next != NULL)
+		b->next->prev = *a;
+	b->prev = (*a)->prev;
+	b->next = *a;
+	if ((*a)->prev != NULL)
+		(*a)->prev->next = b;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = b;
+	(*a)->prev = b;
+	*a = b->prev;
 }
 
 
+
 /**
- * insertion_sort_list  - insertion_sort_list
- * @list: doubly liked list
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers in ascending order
+ * @list: A pointer to the head of the doubly linked list
  *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *prev;
-	int value;
+	listint_t *i, *j, *temp;
 
-	if (list == NULL || (*list)->next == NULL || (*list) == NULL)
-	{
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	}
-	head = *list;
-	while (head)
-	{
-		prev = head->prev;
-				value = head->n;
 
-		while (prev && prev->n > value)
+	for (i = (*list)->next; i != NULL; i = temp)
+	{
+		temp = i->next;
+		j = i->prev;
+		while (j != NULL && i->n < j->n)
 		{
-			swap_nodes(prev, head, list);
-			print_list(*list);
-			prev = head->prev;
+			swap_node(list, &j, i);
+			print_list((const listint_t *)*list);
 		}
-		head = head->next;
+
 	}
+
+
 }
